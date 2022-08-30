941deaca51d2664841f0cae3964944aa71c2d8c4
heywon0909 <hw7319@naver.com>
2022-08-30 22:55:18 +0900

    ui 반응형

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,57 @@
 <template>
   <div class="flex h-screen">
-    <div class="w-1/4  ml-10 mt-5 flex flex-col justify-between">
+    <div class="w-1/6  ml-20 pt-5 flex flex-col justify-between border-r border-blue-80">
       <div>
-        <div class="flex flex-row">
+        <div class="pb-3 w-full flex flex-col lg:inline-block lg:items-start items-center pr-3 mb-6 border-b border-gray-300">
           <i class="fas fa-paw text-3xl fa-fw text-blue-800 ml-4"></i>
-          <span class="ml-2 text-2xl">study</span>
+          <span class="ml-2 text-2xl hidden lg:inline-block">study</span>
         </div>
-    <div class="flex flex-col items-start">
-      <div class="px-4 py-2 hover:bg-gray-300 rounded-full hover:text-blue-800">
-         <i class="fas fa-calendar-alt fa-fw text-2xl"></i>
-         <span class="text-lg ml-3">달력보기</span>
-      </div>
-       <div class="px-4 py-2 hover:bg-gray-300 rounded-full hover:text-blue-800">
-          <i class="fas fa-check-circle fa-fw text-2xl"></i>
-          <span class="text-lg ml-3">오늘의일정</span>
-      </div>
-       <div class="px-4 py-2 hover:bg-gray-300 rounded-full hover:text-blue-800">
-          <i class="fas fa-signal  fa-fw text-2xl"></i>
-          <span class="text-lg ml-3">공부통계</span>
-      </div>
-       <div class="px-4 py-2 hover:bg-gray-300 rounded-full hover:text-blue-800">
-          <i class="far fa-user fa-fw text-2xl"></i>
-          <span class="text-lg ml-3">프로필</span>
-      </div>
-      <div class="px-4 py-2 hover:bg-gray-300 rounded-full hover:text-blue-800">
-          <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
-          <span class="text-lg ml-3">더보기</span>
-      </div>
-    </div>
-      <div class="mr-2">
-        <button class="bg-blue-800 mt-3 text-white rounded-full w-full h-12">
-         일정 추가
-        </button>
-      </div>
-    </div>  
-    <div class="mb-3 mr-3">
-        <button class="bg-gray-50 mt-3 rounded-full w-full h-20 px-3 flex items-center">
-         <img src="http://picsum.photos/100" alt="" class="rounded-full w-12 h-12 mt-3"/>
-         <div class="ml-5 flex flex-col items-start">
-          <div class="text-sm font-bold">박혜원</div>
-          <div class="text-sm text-gray-500">hw7319@naver.com</div>
+        <div class="-ml-2 w-full mb-3 flex flex-col items-center border border-gray-300 rounded-lg">
+          <img src="http://picsum.photos/100" alt="" class="w-full  lg:w-40 h-50 mt-3"/>
+        <button class="mt-3   h-20 px-3 flex items-center">
+         
+         <div class="flex flex-col items-start">
+          <div class="text-sm font-bold hidden lg:block">박혜원</div>
+          <div class="text-sm text-gray-500 hidden lg:block">hw7319@naver.com</div>
          </div>
          <i class="fas fa-ellipsis-h fa-fw text-2xl ml-auto text-sm "></i>
         </button>
+      </div>
+    <div class="flex flex-col lg:items-start items-center">
+      <div class="px-4 py-2 hover:bg-gray-300 rounded-full hover:text-blue-800">
+         <i class="fas fa-calendar-alt fa-fw text-2xl"></i>
+         <span class="text-lg ml-3 hidden lg:inline-block">달력보기</span>
+      </div>
+       <div class="px-4 py-2 hover:bg-gray-300 rounded-full hover:text-blue-800">
+          <i class="fas fa-check-circle fa-fw text-2xl"></i>
+          <span class="text-lg ml-3 hidden lg:inline-block">오늘의일정</span>
+      </div>
+       <div class="px-4 py-2 hover:bg-gray-300 rounded-full hover:text-blue-800">
+          <i class="fas fa-signal  fa-fw text-2xl"></i>
+          <span class="text-lg ml-3 hidden lg:inline-block">공부통계</span>
+      </div>
+       <div class="px-4 py-2 hover:bg-gray-300 rounded-full hover:text-blue-800">
+          <i class="far fa-user fa-fw text-2xl"></i>
+          <span class="text-lg ml-3 hidden lg:inline-block">프로필</span>
+      </div>
+      <div class="px-4 py-2 hover:bg-gray-300 rounded-full hover:text-blue-800">
+          <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
+          <span class="text-lg ml-3 hidden lg:inline-block">더보기</span>
+      </div>
     </div>
+      <div class="flex flex-col items-center lg:mr-3">
+        <button class="bg-blue-800 mt-3 text-white rounded-full lg:w-full h-12 w-12 lg:pr-3">
+         <span class="hidden lg:block"> 일정 추가</span>
+        <i class="fas fa-plus lg:hidden"></i>
+        </button>
+      </div>
+    </div>  
+    
   </div>
-  <div class="flex-1 bg-blue-400">
+  <div class="flex-1">
+    <div class="border-b border-gray-300 w-full mt-9">
+      <span class="text-2xl font-bold ml-3">P.L.A.N</span>
+    </div>
   </div>
   </div>
 </template>
