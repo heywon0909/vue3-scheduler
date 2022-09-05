dc476188708bc1f511f81f5053974a5af8d3cf23
heywon0909 <hw7319@naver.com>
2022-09-05 23:01:10 +0900

    오른쪽 nav 구상중..

--- src/App.vue
+++ src/App.vue
@@ -77,8 +77,24 @@
           
         </div>
       </div>
-      <div class="w-1/3">
-        실천일수
+      <div class="hidden lg:w-1/3 lg:block px-2 py-2">
+        <div class="rounded-xl flex flex-col w-full">
+        <div class="mt-5 px-4 py-2 bg-gray-100 flex items-center justify-between border-b border-gray-300">
+          <span class="font-bold text-lg">나의 공부 순위(지난달)</span>
+          <i class="fas fa-search"></i>
+        </div>
+        <div class="bg-gray-100 px-4 py-2 hover:bg-gray-200" v-for="rank in 5" :key="rank">
+          <div class="flex flex-1 justify-between items-center">
+            <div class="text-xl fond-bold bg-blue-400 w-12 h-12 rounded-full py-3 px-2 text-white">1위</div>
+            <div class= "text-xl ml-3">javascript</div>
+            <div class="text-xl text-gray-500">80%</div>
+          </div>
+        </div>
+        <div class="bg-gray-100 px-4 py-2  hover:bg-gray-200 flex justify-between">
+          <div class="font-bold">더보기</div>
+          <i class="fas fa-ellipsis-h"></i>
+        </div>
+      </div>
       </div>
     </div>
     <!-- <div class="border-b border-gray-300 w-full mt-9">
