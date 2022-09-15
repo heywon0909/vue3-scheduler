45e01c2ec5008cce326b16514f41ba31300c733c
heywon0909 <hw7319@naver.com>
2022-09-15 23:00:45 +0900

    달력제작중...

--- src/pages/Calendar.vue
+++ src/pages/Calendar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex-1 flex">
-       <div class=" w-3/5 border-r border-blue-80">
+       <div class="flex-1 w-full border-r border-blue-80">
         <div class="flex flex-col mt-5">
           <div class="w-full">
             <div class="text-2xl border-b border-blue-80 px-2 py-2 h-19">calendar</div>
@@ -11,22 +11,29 @@
           <div class="font-bold text-xl">2022.09</div>
           <div class="text-gray-500 ml-2"> next </div>
          </div>
-         <div class="flex items-center justify-center flex-1">
-           <div class="flex space-x-5">
-            <div>MON</div>
-            <div>TUE</div>
-            <div>WED</div>
-            <div>THU</div>
-            <div>FRI</div>
-            <div>SAT</div>
-            <div>SUN</div>
+         <div class="flex items-center justify-center flex-1 border-b border-gray-80">
+           <div class="flex-1 flex justify-between mr-3 ml-3 p-3">
+            <div class="">MON</div>
+            <div class="">TUE</div>
+            <div class="">WED</div>
+            <div class="">THU</div>
+            <div class="">FRI</div>
+            <div class="">SAT</div>
+            <div class="">SUN</div>
            </div>
+         </div>
+         <div class="flex flex-1 justify-between mr-3 ml-3 px-3 border-b border-blue-80">
+            <div class="w-1/7 border-r border-blue-80">1</div>
+            <div class="w-1/7 border-r border-blue-80">2</div>
+            <div class="w-1/7 border-r border-blue-80">3</div>
+            <div class="w-1/7 border-r border-blue-80">4</div>
+            <div class="w-1/7 border-r border-blue-80">5</div>
+            <div class="w-1/7 border-r border-blue-80">6</div>
+            <div class="w-1/7 border-r border-blue-80">7</div>
          </div>
        </div>
        
-       <div class="w-2/5">
-
-       </div>
+       
       
     
     <!-- <div class="border-b border-gray-300 w-full mt-9">
